e5ab56d4253b89d813ef8ac41c8674a713e996af
Jeffrey Daniel <jdaniel6@stevens.edu>
2022-11-16 21:24:35 -0500

    updates HW15
    updates the Game of Life to be semi-functional. Next build will hopefully make it fully functional

--- HW15 Game of Life/Daniel_Jeffrey_HW9.cc
+++ HW15 Game of Life/Daniel_Jeffrey_HW9.cc
@@ -18,9 +18,11 @@ class Exception{
 
 class Life{
     private:
-        uint8_t* board;
+        uint16_t* board;
         uint64_t rows;
         uint64_t cols;
+    private:
+        uint64_t get_count_live_cells(uint64_t row, uint64_t col, uint64_t current_position);
     public:
         Life();
         Life(uint64_t num_of_rows, uint64_t num_of_cols);
@@ -67,20 +69,20 @@ int main(){
 
 //default constructors
 Life::Life(){
-    board = new uint8_t[0];
+    board = new uint16_t[0];
     rows = 0;
     cols = 0;
 }
 
 Life::Life(uint64_t num_of_rows, uint64_t num_of_cols){
-    board = new uint8_t[num_of_rows * num_of_cols];
+    board = new uint16_t[num_of_rows * num_of_cols];
     rows = num_of_rows;
     cols = num_of_cols;
 }
 
 //copy constructor
 Life::Life(Life &old) : rows(old.rows), cols(old.cols) {
-    this->board = new uint8_t[rows * cols];
+    this->board = new uint16_t[rows * cols];
     for(uint64_t i = 0; i < rows*cols; i++){
         board[i] = old.board[i];
     }
@@ -104,7 +106,7 @@ void Life::read(std::string file_name){
     std::ifstream is(file_name);
     uint16_t element;
     is >> rows >> cols;
-    board = new uint8_t[rows * cols];
+    board = new uint16_t[rows * cols];
     uint64_t pos = 0;
     while(is >> element){        
         board[pos] = element;
@@ -117,9 +119,83 @@ void Life::read(uint8_t* board){
     //to be implemented
 }
 
+//get number of live cells around a particular cell
+uint64_t Life::get_count_live_cells(uint64_t row, uint64_t col, uint64_t current_position){
+    if(row == 0){
+        if(col == 0){   //first element in the first row, has only 3 neighbours
+            return (board[1] + board[cols] + board[cols+1]);
+        }
+        else{
+            if(col == (cols-1)){    //last element in first row has only 3 neighbours
+                return (board[cols-2] + board[2*cols - 2] + board[2*cols - 1]);
+            }
+            else{   //all other elements in first row have 5 neighbours
+                return (board[current_position - 1] + board[current_position + cols - 1] + board[current_position + cols] + board[current_position + cols + 1] + board[current_position + 1]);
+            }
+        }
+    }
+    else{
+        if(row == (rows-1)){
+            if(col == 0){   //first element in the last row, only has 3 neighbours
+                return(board[(rows-1)*cols + 1] + board[(rows-2)*cols] + board[(rows-2)*cols + 1]);
+            }
+            else{
+                if(col == (cols-1)){    //last element in last row has only 3 neighbours
+                    return(board[rows*cols - 2] + board[(rows-1)*cols - 1] + board[(rows-1)*cols - 2]);
+                }
+                else{   //all other elements in last row have 5 neighbours
+                    return (board[current_position - 1] + board[current_position - cols - 1] + board[current_position - cols] + board[current_position - cols + 1] + board[current_position + 1]);
+                }
+            }
+        }
+        else{
+            if((col%cols) == 0){    //first column has 5 neighbours (top and bottom have already been taken care of)
+                return(board[current_position - cols] + board[current_position - cols + 1] + board[current_position + 1] + board[current_position + cols] + board[current_position + cols + 1]);
+            }
+            else{
+                if((col%cols) == (cols-1)){ //last column has 5 neighbours (top and bottom have already been taken care of)
+                    return(board[current_position - cols] + board[current_position - cols - 1] + board[current_position - 1] + board[current_position + cols - 1] + board[current_position + cols]);
+                }
+                else{   //all edgecases have been dealt with; all other cells have 8 neighbours
+                    return(board[current_position - cols - 1] + board[current_position - cols] + board[current_position - cols + 1] + board[current_position + 1] + board[current_position + cols + 1] + board[current_position + cols] + board[current_position + cols - 1] + board[current_position - 1]);
+                }
+            }
+        }
+    }
+}
+
 //get next generation of the current board
 void Life::get_next_gen(){
-
+    uint16_t* next_board = new uint16_t[rows*cols];
+    for(uint64_t i = 0; i < rows; i++){
+        for(uint64_t j = 0; j < cols; j++){
+            uint16_t current_state = board[i * cols + j];
+            uint16_t count = get_count_live_cells(i, j, current_state);
+            if(current_state == 1){
+                if((count == 2) || (count == 3)){
+                    next_board[current_state] = 1;
+                }
+                else{
+                    next_board[current_state] = 0;
+                }
+            }
+            else{
+                if(current_state == 0){
+                    if(count == 3){
+                        next_board[current_state] = 1;
+                    }
+                    else{
+                        next_board[current_state] = 0;
+                    }
+                }
+                else{
+                    throw new Exception("InvalidDataException", __FILE__, __LINE__);
+                }
+            }
+        }
+    }
+    delete [] board;
+    board = next_board;
 }
 
 //get a certain generation of the current board
@@ -131,7 +207,7 @@ void Life::get_gen(uint64_t generation_number){
 void Life::print(){
     for(uint64_t i = 0; i < rows; i++){
         for(uint64_t j = 0; j < cols; j++){
-            std::cout << (uint16_t)board[i * cols + j] << "\t";
+            std::cout << board[i * cols + j] << "\t";
         }
         std::cout << "\n";
     }
